f409b3f596070373ebfa5b528c224cd2ac049cef
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-18 17:11:01 +0200

    NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BiExit } from 'react-icons/bi';
+import InputLang from '../Input/InputLang';
+
+
+const NavBar = () => {
+    return (
+        <>
+            <BiExit />
+            <p>Увійти</p>
+            <InputLang />
+        </>
+    )
+   
+    
+}
+
+export default NavBar
