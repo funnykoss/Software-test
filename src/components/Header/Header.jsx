b6ada6c4c5f61786f80f736f7048fd4dd28182cc
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-18 17:11:12 +0200

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "../Button/Button";
+import Container from "../Conteiner/Container";
+import HeaderLogo from "../HeaderLogo/HeaderLogo";
+import NavBar from "../NavBar";
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={s.section}>
+            <Container >
+                <div className={s.header}>
+            <HeaderLogo />
+            <div className={s.navigate}>
+                <Button/>
+                <NavBar />
+            </div>
+         </div>   
+       </Container>
+
+        </div>
+       
+    )
+}
+
+export default Header
